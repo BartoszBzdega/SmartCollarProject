52351940311dfe52a2a506d4ed18bbb265d6a9a1
MariaGrzelak20 <mariamgrzelak@gmail.com>
2024-11-15 10:18:14 +0100

    Information prewiev for data in app was added, constraint's were set for map fragment.

--- app/src/main/java/com/adafruit/bluefruit/le/connect/app/PeripheralModulesFragment.java
+++ app/src/main/java/com/adafruit/bluefruit/le/connect/app/PeripheralModulesFragment.java
@@ -60,7 +60,7 @@ public class PeripheralModulesFragment extends ConnectedPeripheralFragment {
     private final static int MODULE_CIRCUITPYTHON = 9;
     private final static int MODULE_DFU = 10;
     private final static int MODULE_MAP = 11;
-
+    private final static int MODULE_INFORMATION_PREVIEW = 12;
 
     // Data
     private PeripheralModulesFragmentListener mListener;
@@ -276,6 +276,10 @@ public class PeripheralModulesFragment extends ConnectedPeripheralFragment {
             case MODULE_MAP: // TO DODAJE INSTANCJE FRAGMENTU (Czyli przycisk)
                 fragment = MapFragment.newInstance(singlePeripheralIdentifier);
                 break;
+
+            case MODULE_INFORMATION_PREVIEW:
+                fragment = InformationPreview.newInstance(singlePeripheralIdentifier);
+                break;
         }
 
         if (fragment != null && mListener != null) {
@@ -475,6 +479,11 @@ public class PeripheralModulesFragment extends ConnectedPeripheralFragment {
                             iconDrawableId = R.drawable.tab_dfu_icon; // TODO: ZMIENIC IKONE
                             titleId = R.string.map_tab_title;
                             break;
+
+                        case MODULE_INFORMATION_PREVIEW:
+                            iconDrawableId = R.drawable.tab_dfu_icon; // TODO: ZMIENIC IKONE
+                            titleId = R.string.informationPreview;
+                            break;
                     }
 
                     ModuleViewHolder moduleViewHolder = (ModuleViewHolder) holder;
@@ -518,7 +527,7 @@ public class PeripheralModulesFragment extends ConnectedPeripheralFragment {
 
                 List<Integer> modules = new ArrayList<>(Collections.singletonList(MODULE_INFO));
                 if (hasUart) {
-                    modules.addAll(Arrays.asList(MODULE_UART, MODULE_PLOTTER, MODULE_PINIO, MODULE_CONTROLLER, MODULE_NEOPIXEL, MODULE_THERMALCAMERA, MODULE_IMAGETRANSFER, MODULE_MAP));
+                    modules.addAll(Arrays.asList(MODULE_UART, MODULE_PLOTTER, MODULE_PINIO, MODULE_CONTROLLER, MODULE_NEOPIXEL, MODULE_THERMALCAMERA, MODULE_IMAGETRANSFER, MODULE_MAP, MODULE_INFORMATION_PREVIEW));
                 }
                 if (hasCircuitPython) {
                     modules.add(MODULE_CIRCUITPYTHON);
