a7df1bfdebd9eb8490ecb25f8f653ca6fa94c018
Michał Szczeciński <163310113+reddosqb@users.noreply.github.com>
2024-11-22 10:03:34 +0100

    file type fix

--- gps_module/gps_module.ino
+++ gps_module/gps_module.ino
@@ -1,79 +1,64 @@
 #include <Adafruit_GPS.h>
-// what's the name of the hardware serial port?
+
+// Define the hardware serial port for GPS communication
 #define GPSSerial Serial1
+
 // Connect to the GPS on the hardware port
 Adafruit_GPS GPS(&GPSSerial);
-// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
-// Set to 'true' if you want to debug and listen to the raw GPS sentences
+
+// Set GPSECHO to 'false' to disable raw GPS data echo for debugging
 #define GPSECHO false
+
 uint32_t timer = millis();
 float gpspacket[2];
-const char * lon = "";
-const char * lat = "";
-void setup() {
-  // put your setup code here, to run once:
-  //while (!Serial);  // uncomment to have the sketch wait until Serial is ready
+char lon[20]; // For formatted longitude
+char lat[20]; // For formatted latitude
 
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-  // also spit it out
+void setup() {
+  // Initialize serial for debugging
   Serial.begin(115200);
   Serial.println("Adafruit GPS library basic parsing test!");
 
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  // Initialize GPS at 9600 baud (default for Adafruit MTK GPS)
   GPS.begin(9600);
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
 
-  // Request updates on antenna status, comment out to keep quiet
-  GPS.sendCommand(PGCMD_ANTENNA);
+  // Configure GPS output and update rate
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // RMC and GGA sentences
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);    // 1 Hz update rate
+  GPS.sendCommand(PGCMD_ANTENNA);               // Request antenna status
 
   delay(1000);
-
-  // Ask for firmware version
-  GPSSerial.println(PMTK_Q_RELEASE);
+  GPSSerial.println(PMTK_Q_RELEASE); // Request firmware version
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
-  if (GPSECHO)
-    if (c) Serial.print(c);
-  // if a sentence is received, we can check the checksum, parse it...
+  if (GPSECHO && c) Serial.print(c);
+
+  // Check for and parse new NMEA sentences
   if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    //Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-      return; // we can fail to parse a sentence in which case we should just wait for another
+    if (!GPS.parse(GPS.lastNMEA())) return;
   }
 
-  // approximately every 2 seconds or so, print out the current stats
+  // Print GPS data every 5 seconds
   if (millis() - timer > 5000) {
-    timer = millis(); // reset the timer
-   
+    timer = millis();
 
-    //Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    //Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-    gpspacket[1] = GPS.latitude;
-    gpspacket[2] = GPS.longitude;
-    //Serial.print(" " + gpspacket);
-    dtostrf(gpspacket[1], 1, 14, lat);
-    dtostrf(gpspacket[2], 1, 14, lon);
-    Serial.println("##########");
-    Serial.println("lattitude");
-    Serial.println(lat);
-    Serial.println("longitude ");
-    Serial.println(lon);
+    //if (GPS.fix) {
+      gpspacket[0] = GPS.latitude;
+      gpspacket[1] = GPS.longitude;
 
+      dtostrf(gpspacket[0], 1, 14, lat);
+      dtostrf(gpspacket[1], 1, 14, lon);
+
+      Serial.println("##########");
+      Serial.println("Latitude:");
+      Serial.println(lat);
+      Serial.println("Longitude:");
+      Serial.println(lon);
+    //} else {
+      Serial.println("Waiting for GPS fix...");
+    }
   }
-}
+//}
+
