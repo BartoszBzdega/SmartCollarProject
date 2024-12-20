f580d8b11bd3d7e091e03a9ad0fc62870b521324
MariaGrzelak20 <mariamgrzelak@gmail.com>
2024-12-20 09:22:30 +0100

    reading data from JSon (just reading, not all of it)

--- app/src/main/java/com/adafruit/bluefruit/le/connect/app/DataStorage.java
+++ app/src/main/java/com/adafruit/bluefruit/le/connect/app/DataStorage.java
@@ -7,14 +7,22 @@ import androidx.core.content.ContextCompat;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.File;  // Import the File class
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
 public class DataStorage {
 
@@ -76,9 +84,9 @@ public class DataStorage {
                 JSONObject jobject = new JSONObject();
                 JSONArray jarray = new JSONArray();
                 JSONObject jvalues = new JSONObject();
-                jvalues.put("Date", data);
-                jvalues.put("Distance", walkDistance);
-                jvalues.put("Time", walkTime);
+                jvalues.put("WalkDate", data);
+                jvalues.put("WalkDistance", walkDistance);
+                jvalues.put("WalkTime", walkTime);
 
                 jarray.put(jvalues);
                 jobject.put("Walk Data", jarray);
@@ -146,7 +154,57 @@ public class DataStorage {
         }
     }
 
-    public void readWalkData(){}
+    public String loadJSONFromAsset(File file) {
+        String json = null;
+        try {
+            FileInputStream fis = new FileInputStream(file);
+
+            int size = fis.available();
+            byte[] buffer = new byte[size];
+            fis.read(buffer);
+            fis.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    public ArrayList readWalkData(File path){
+        File plik =new File(path,fileName);
+        ArrayList<HashMap<String, String>> formList = new ArrayList<>();
+       try{
+       if(plik.exists())
+       {
+               String json;
+
+               JSONObject obj = new JSONObject(loadJSONFromAsset(plik));
+               JSONArray jArry = obj.getJSONArray("Walk Data");
+               formList = new ArrayList<HashMap<String, String>>();
+               HashMap<String, String> m_li;
+
+               for (int i = 0; i < jArry.length(); i++) {
+                   JSONObject jo_inside = jArry.getJSONObject(i);
+                   Log.d("Details-->", jo_inside.getString("WalkDate"));
+                   String formula_value = jo_inside.getString("WalkDate");
+                   //String url_value = jo_inside.getString("Time");
+
+                   //Add your values in your `ArrayList` as below:
+                   m_li = new HashMap<String, String>();
+                   m_li.put("Date", formula_value);
+                   //m_li.put("url", url_value);
+
+                   formList.add(m_li);
+
+               }
+       return formList;
+       }
+       } catch (Exception e) {
+           throw new RuntimeException(e);
+       }
+    return formList;
+    }
 
     public void readPetInfo(){}
 }
