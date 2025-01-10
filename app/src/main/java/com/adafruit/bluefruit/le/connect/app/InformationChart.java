e266f0e4c15cfc9a2863fb0cbeab74318fd0330e
MariaGrzelak20 <mariamgrzelak@gmail.com>
2025-01-10 10:35:10 +0100

    chart beginings

---
+++ app/src/main/java/com/adafruit/bluefruit/le/connect/app/InformationChart.java
@@ -0,0 +1,23 @@
+package com.adafruit.bluefruit.le.connect.app;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.adafruit.bluefruit.le.connect.R;
+import com.adafruit.bluefruit.le.connect.ble.central.UartDataManager;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class InformationChart {
+}
